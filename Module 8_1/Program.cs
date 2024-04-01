3a7f0757e3e91d946265d73d0195063cf417822a
karpov2713 <karpov2713@gmail.com>
2024-04-02 01:55:26 +0300

    Добавьте файлы проекта.

---
+++ Module 8_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_8_1
+{
+    class Disk
+    {
+        public Disk(string name, long totalSpace, long availableSpace)
+        {
+            Name = name;
+            Size = totalSpace;
+            AvailableSpace = availableSpace;
+        }
+
+        public string Name { get; }
+        public long Size { get; }
+        public long AvailableSpace { get; }
+    }
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
