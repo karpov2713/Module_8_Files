8738257ed0aa1d8051e867029c51a2f6cc931a7c
karpov2713 <karpov2713@gmail.com>
2024-04-02 03:51:14 +0300

    Завершена часть 8.2.

--- Module 8_1/Program.cs
+++ Module 8_1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +25,7 @@ namespace Module_8_1
     public class Folder
     {
         public List<string> Files { get; set; } = new List<string>();
-    
+
         Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
 
         public void CreateFolder(string name)
@@ -37,6 +39,109 @@ namespace Module_8_1
     {
         static void Main(string[] args)
         {
+            // получим системные диски
+            /*DriveInfo[] drives = DriveInfo.GetDrives();*/
+
+            /*foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.AvailableFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }*/
+
+            //GetCatalogs();
+
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo("C:\\");
+
+                Console.WriteLine($"Название каталога: {dirInfo.Name}");
+                Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+                Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+                Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine($"Количество директорий = {dirInfo.GetDirectories().Length}");
+                    Console.WriteLine($"Количество файлов = {dirInfo.GetFiles().Length}");
+                }
+
+                DirectoryInfo newDirectory = new DirectoryInfo("C:\\newDirectory");
+
+                if (!newDirectory.Exists)
+                {
+                    newDirectory.Create();
+                }
+
+                Console.WriteLine($"Количество директорий = {dirInfo.GetDirectories().Length}");
+                Console.WriteLine($"Количество файлов = {dirInfo.GetFiles().Length}");
+
+                /*DirectoryInfo delNewDirectory = new DirectoryInfo("C:\\newDirectory");
+
+                if (delNewDirectory.Exists)
+                {
+                    delNewDirectory.Delete(true);
+                    Console.WriteLine("Каталог удален");
+                }
+
+                Console.WriteLine($"Количество директорий = {dirInfo.GetDirectories().Length}");
+                Console.WriteLine($"Количество файлов = {dirInfo.GetFiles().Length}");*/
+
+                // Перемещение каталога
+                DirectoryInfo moveNewDirectory = new DirectoryInfo("C:\\newDirectory");
+                string newPath = "C:\\moveNewDirectory";
+
+                if (moveNewDirectory.Exists && !Directory.Exists(newPath))
+                {
+                    moveNewDirectory.MoveTo(newPath);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+        /*static void GetCatalogs()
+        {
+            string dirName = "C:\\"; // Прописываем путь к корневой директории
+            int numberOfDirs = 0;
+            int numberOfFiles = 0;
+
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                
+                // Получим все директории корневого каталоги
+                foreach (string dir in dirs) // Выведем их все
+                {
+                    Console.WriteLine(dir);
+
+                    numberOfDirs++;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+
+                string[] files = Directory.GetFiles(dirName);
+                
+                //Получим все файлы корневого каталога
+                foreach(string s in files)
+                {
+                    Console.WriteLine(s);
+                    numberOfFiles++;
+                }
+            }
+
+            Console.WriteLine($"Количество директорий = {numberOfDirs}");
+            Console.WriteLine($"Количество файлов = {numberOfFiles}");
+        }*/
     }
 }
